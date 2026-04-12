729d59c79b48a84b939cfefc717fba7ec0e5d970
Roma <RomaLytar@gmail.com>
2026-04-12 12:33:20 +0200

    Add QueueMetricsDriver interface and NullMetricsDriver (#16)

---
+++ tests/Unit/Infrastructure/Metrics/NullMetricsDriverTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yammi\JobsMonitor\Tests\Unit\Infrastructure\Metrics;
+
+use PHPUnit\Framework\TestCase;
+use Yammi\JobsMonitor\Application\Contract\QueueMetricsDriver;
+use Yammi\JobsMonitor\Infrastructure\Metrics\NullMetricsDriver;
+use Yammi\JobsMonitor\Tests\Support\QueueMetricsDriverContractTests;
+
+final class NullMetricsDriverTest extends TestCase
+{
+    use QueueMetricsDriverContractTests;
+
+    protected function createDriver(): QueueMetricsDriver
+    {
+        return new NullMetricsDriver();
+    }
+
+    public function test_get_queue_size_always_returns_null(): void
+    {
+        self::assertNull((new NullMetricsDriver())->getQueueSize('default'));
+        self::assertNull((new NullMetricsDriver())->getQueueSize('emails'));
+    }
+
+    public function test_get_delayed_size_always_returns_null(): void
+    {
+        self::assertNull((new NullMetricsDriver())->getDelayedSize('default'));
+        self::assertNull((new NullMetricsDriver())->getDelayedSize('emails'));
+    }
+
+    public function test_get_reserved_size_always_returns_null(): void
+    {
+        self::assertNull((new NullMetricsDriver())->getReservedSize('default'));
+        self::assertNull((new NullMetricsDriver())->getReservedSize('emails'));
+    }
+}
